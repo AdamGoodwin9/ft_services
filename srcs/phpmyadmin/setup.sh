e7247fae9e34e3258292503320c048d7978e3a01
Adam Goodwin <adamgoodwin12345@gmail.com>
2021-10-24 14:09:19 -0700

    changed php to nginx

--- srcs/phpmyadmin/setup.sh
+++ srcs/phpmyadmin/setup.sh
@@ -2,4 +2,8 @@
 /usr/bin/telegraf &
 
 # Run PHP specifying port and file location
-/usr/bin/php -S 0.0.0.0:80 -t /www/
+#/usr/bin/php -S 0.0.0.0:80 -t /www/
+
+ /usr/sbin/php-fpm7 &
+
+nginx -g 'daemon off;'
--- srcs/wordpress/setup.sh
+++ srcs/wordpress/setup.sh
@@ -2,4 +2,7 @@
 /usr/bin/telegraf &
 
 # Run PHP specifying port and file location
-/usr/bin/php -S 0.0.0.0:80 -t /www/
+#/usr/bin/php -S 0.0.0.0:80 -t /www/
+ /usr/sbin/php-fpm7 &
+
+nginx -g 'daemon off;'
