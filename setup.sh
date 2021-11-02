8568bad63e773d6d8cce82d6e945e88d960d472e
Assxios <droge@student.42.fr>
2021-11-02 11:45:04 -0700

    ready for correction

--- setup.sh
+++ setup.sh
@@ -1,38 +1,32 @@
-./srcs/vm_setup.sh
-
 minikube delete
-minikube start --driver=docker --cpus=2
+minikube start --driver=docker
 
 eval $(minikube docker-env)
-minikube addons enable metrics-server
-minikube addons enable dashboard &> /dev/null
 minikube addons enable metallb
 
-while true; do
-  kubectl -n kube-system get pod | grep -i Pending 1> /dev/null 2>&1
-  if [ $? == 0 ] ; then
-     echo "Cluster is not ready, sleeping"
-    sleep 1
-  else
-    break
-  fi
-done
+#while true; do
+#  kubectl -n kube-system get pod | grep -i Pending 1> /dev/null 2>&1
+#  if [ $? == 0 ] ; then
+#     echo "Cluster is not ready, sleeping"
+#    sleep 1
+#  else
+#    break
+#  fi
+#done
 
 
-docker build -t nginx_alpine srcs/nginx
-docker build -t wordpress_alpine srcs/wordpress
-docker build -t mysql_alpine srcs/mysql
-docker build -t phpmyadmin_alpine srcs/phpmyadmin
-docker build -t ftps_alpine srcs/ftps
-docker build -t grafana_alpine srcs/grafana
-docker build -t influxdb_alpine srcs/influxdb
+docker build -t nginx_alpine srcs/nginx &> /dev/null
+docker build -t wordpress_alpine srcs/wordpress &> /dev/null
+docker build -t mysql_alpine srcs/mysql &> /dev/null
+docker build -t phpmyadmin_alpine srcs/phpmyadmin &> /dev/null
+docker build -t ftps_alpine srcs/ftps &> /dev/null
+docker build -t grafana_alpine srcs/grafana &> /dev/null
+docker build -t influxdb_alpine srcs/influxdb &> /dev/null
 
 kubectl apply -f srcs/yaml_metallb/metallb.yaml &> /dev/null
 kubectl apply -f srcs/yaml_volumes/influxdb.yaml &> /dev/null
 kubectl apply -f srcs/yaml_deployments/influxdb.yaml &> /dev/null
 kubectl apply -f srcs/yaml_services/influxdb.yaml &> /dev/null
-sleep 8
-
 kubectl apply -f srcs/yaml_volumes/mysql.yaml &> /dev/null
 
 kubectl apply -f srcs/yaml_deployments/nginx.yaml &> /dev/null
@@ -49,12 +43,9 @@ kubectl apply -f srcs/yaml_services/phpmyadmin.yaml &> /dev/null
 kubectl apply -f srcs/yaml_services/ftps.yaml &> /dev/null
 kubectl apply -f srcs/yaml_services/grafana.yaml &> /dev/null
 
-echo "-----------------------------------------------------------------------------------------------------------------"
-echo "| Services      | SSH nginx     | PHPMyAdmin    | InfluxDB      | FTPS          | Wordpress     | Grafana       |"
-echo "|---------------------------------------------------------------------------------------------------------------|"
-echo "| Login         | ssh_admin     | admin         | graf_admin    | ftp_admin     | cclaude       | admin         |"
-echo "| Password      | 0101          | 12345         | 10101         | 01010         | cclaude1      | admin         |"
-echo "-----------------------------------------------------------------------------------------------------------------\n"
-echo "Open 172.17.0.2 in a web browser"
-kubectl get svc
-echo
+#PhpMyAdmin: wp_admin 1010"
+#Wordpress: agoodwin 12345"
+#grafana: admin admin"
+#ftps: ftp_admin 01010"
+
+minikube dashboard &
